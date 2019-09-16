4035d9d56bae092bd796ae91286bda3b09b0ece1
DESKTOP-5DTDNTA\ASUS <phatsngoo2702@gmail.com>
2019-09-16 23:06:24 +0700

    First commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+﻿--create Database [Blogging]
+--go
+--use [Blogging]
+--go
+create table [Blogger](
+	[BloggerID] int not null identity,
+	[BloggerName] nvarchar(50) not null,
+	[FacebookURL] nvarchar(max) not null
+	CONSTRAINT [Blogger_PK] PRIMARY KEY ([BloggerID])
+);
+create table [Post](
+	[PostID] int not null identity,
+	[BloggerID] int not null,
+	[Content] nvarchar(max),
+	[Title] nvarchar(max),
+	CONSTRAINT [PK_POST] PRIMARY KEY ([PostID]),
+	CONSTRAINT [PK_BLOGGER_ID] FOREIGN KEY ([BloggerID]) References [Blogger] ([BloggerID])  ON DELETE CASCADE
+);
+select* from Blogger
